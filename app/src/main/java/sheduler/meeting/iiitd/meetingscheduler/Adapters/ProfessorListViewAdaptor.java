b561f2414788ac617c1fb15fe8a50c1871558b24
Mansi Vijh <mansivijh94@gmail.com>
2015-04-30 19:56:09 +0530

    changed package name

---
+++ app/src/main/java/sheduler/meeting/iiitd/meetingscheduler/Adapters/ProfessorListViewAdaptor.java
@@ -0,0 +1,62 @@
+package sheduler.meeting.iiitd.meetingscheduler.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import sheduler.meeting.iiitd.meetingscheduler.PopulatingClass.ProfessorDetails;
+import sheduler.meeting.iiitd.meetingsheduler.R;
+
+/**
+ * Created by Masood on 4/28/2015.
+ */
+public class ProfessorListViewAdaptor extends BaseAdapter {
+
+
+    ArrayList<ProfessorDetails> professorDetails =new ArrayList<ProfessorDetails>();
+   // Context context;
+    LayoutInflater inflater;
+
+    TextView name, course;
+
+    public ProfessorListViewAdaptor(ArrayList<ProfessorDetails> professorDetails, Context context) {
+        this.professorDetails = professorDetails;
+    //    this.context = context;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return professorDetails.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView=inflater.inflate(R.layout.activity_professor_list_view_row, parent, false);
+
+        name=(TextView) convertView.findViewById(R.id.professor_lv_row_name);
+        course=(TextView) convertView.findViewById(R.id.professor_lv_row_course);
+
+        name.setText(professorDetails.get(position).getProfName());
+        course.setText(professorDetails.get(position).getCourse());
+
+
+        return convertView;
+    }
+}
